18e5ab0a4c17e9735852bdc0d8b0bf4da2974917
hairy-snake <mamaev.gosha99@gmail.com>
2025-04-06 23:00:45 +0500

    some fixes

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract MultiSigWallet is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+contract MultiSigWallet is Initializable, OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuard {
     address[] public owners;
     uint256 public required;
     uint256 public transactionsCount;
